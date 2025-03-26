ed2f0c18b629a23c578e45b113df5cc0fb04e6da
Dedik Kurniawan <96458949+dedik2urniawan@users.noreply.github.com>
2025-03-26 12:37:48 +0700

    Initial composite_analysis.py

---
+++ composite_analysis.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show_dashboard():
+    st.markdown("### Analisis Composite")
+    st.info("Fitur ini sedang dalam tahap pengembangan dan akan segera diluncurkan. Kami menghargai kesabaran Anda dalam menunggu pembaruan ini.")
