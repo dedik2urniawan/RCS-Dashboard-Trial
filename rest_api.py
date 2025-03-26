3fef469fb07c6b5d49c991a794a67ad5279cbe9b
Dedik Kurniawan <96458949+dedik2urniawan@users.noreply.github.com>
2025-03-26 12:38:56 +0700

    Initial rest_api.py

---
+++ rest_api.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show_dashboard():
+    st.markdown("### API Integrasi")
+    st.info("Fitur ini sedang dalam tahap pengembangan dan akan segera diluncurkan. Kami menghargai kesabaran Anda dalam menunggu pembaruan ini.")
